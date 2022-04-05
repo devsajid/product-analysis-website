97e05331a042943cfe8cdc75985acc960e188713
devsajid <74415469+devsajid@users.noreply.github.com>
2022-04-05 10:20:24 +0600

    Navbar complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,9 @@
+import React from "react";
+import Header from "./Components/Header/Header";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-6xl font-bold underline">Hello world!</h1>
+      <Header></Header>
     </div>
   );
 }
